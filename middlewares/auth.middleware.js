e6741d36b61b9e241d15afd7c3cbceb93d45ca03
srseen <seereen.2540@gmail.com>
2025-03-30 13:16:53 +0700

    feat: add authentication middleware for token verification

---
+++ middlewares/auth.middleware.js
@@ -0,0 +1,17 @@
+const jwt = require("jsonwebtoken");
+
+const authMiddleware = (req, res, next) => {
+  const token = req.header("Authorization");
+  if (!token) {
+    return res.status(403).send({ sucsess: false, message: "Access Denied" });
+  }
+  try {
+    const verified = jwt.verify(token, process.env.JWT_SECRET);
+    req.user = verified;
+    next();
+  } catch (err) {
+    res.status(400).send({ sucsess: false, message: "Invalid Token", err });
+  }
+};
+
+module.exports = authMiddleware;
